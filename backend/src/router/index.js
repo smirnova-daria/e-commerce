208f54a06f53dcc6fce7767a6cefafa3c510edef
Daria Smirnova <darya.smirn1@yandex.ru>
2023-07-18 18:55:13 +0300

    chore: add forms for login, request password, reset password

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from '../views/Login.vue'
 import Dashboard from '../views/Dashboard.vue'
+import RequestPassword from '../views/RequestPassword.vue'
+import ResetPassword from '../views/ResetPassword.vue'
 
 const routes = [
     {
@@ -12,6 +14,16 @@ const routes = [
         path: '/login',
         name: 'login',
         component: Login
+    },
+    {
+        path: '/request-password',
+        name: 'request-password',
+        component: RequestPassword
+    },
+    {
+        path: '/reset-password',
+        name: 'reset-password',
+        component: ResetPassword
     },
 ]
 
